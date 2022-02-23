ecebb46c80020df4b0e13446d4e4b70662566f95
Gelson Joao <gelson.joao@jupiter.co.ao>
2022-02-23 18:38:09 +0100

    Criação do Menu principal de Navegação

--- portalModel/src/app/app.module.ts
+++ portalModel/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home/home.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    MainMenuComponent
   ],
   imports: [
     BrowserModule,
