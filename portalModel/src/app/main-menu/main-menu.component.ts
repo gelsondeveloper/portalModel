d8c1687dc092d8ce82a46206681cf0f0bcd4ffab
Gelson Joao <gelson.joao@jupiter.co.ao>
2022-03-04 15:36:28 +0100

    termino da estrutura de Menu MINFIN

--- portalModel/src/app/main-menu/main-menu.component.ts
+++ portalModel/src/app/main-menu/main-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-main-menu',
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class MainMenuComponent implements OnInit {
 
   constructor() { }
-
+  @Output() active = true;
   ngOnInit(): void {
   }
 
